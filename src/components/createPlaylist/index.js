b2c4085a66202241fd5fba5885c67026cc6df961
marufboy <afif23ixa@gmail.com>
2022-04-13 23:25:58 +0800

    add materialUi in components

--- src/components/createPlaylist/index.js
+++ src/components/createPlaylist/index.js
@@ -1,11 +1,24 @@
-const CreatePlaylist = ({playlist, handleSubmit, handleFormChange}) => (
-    <form className="container" onSubmit={handleSubmit}>
-        <label htmlFor="title" className="titleLabel">Title</label>
-        <input name="name" value={playlist.name} onChange={handleFormChange}/>
-        <label htmlFor="desc" className="titleLabel">Description</label>
-        <input name="description" value={playlist.description} onChange={handleFormChange}/>
-        <button type="submit">Create Playlist</button>
-    </form>
+const CreatePlaylist = ({ playlist, handleSubmit, handleFormChange }) => (
+  <form className="container-form" onSubmit={handleSubmit}>
+    <label htmlFor="name" className="titleLabel">
+      Playlist Name
+    </label>
+    <input name="name" value={playlist.name} onChange={handleFormChange} />
+    <label htmlFor="description" className="titleLabel">
+      Description
+    </label>
+    <textarea
+      rows="4"
+      cols="30"
+      name="description"
+      value={playlist.description}
+      onChange={handleFormChange}
+      style={{resize: 'none'}}
+    />
+    <button className="neutral-btn" type="submit">
+      Create Playlist
+    </button>
+  </form>
 );
 
 export default CreatePlaylist;
